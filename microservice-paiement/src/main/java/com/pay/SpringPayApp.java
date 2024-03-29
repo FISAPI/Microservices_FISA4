626fe533192d747e42cf84574ced06420bb79c3a
Léo Wadin <wadinleo@gmail.com>
2024-03-29 14:36:17 +0100

    fix: package names

---
+++ microservice-paiement/src/main/java/com/pay/SpringPayApp.java
@@ -0,0 +1,12 @@
+package com.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPayApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPayApp.class, args);
+	}
+}
