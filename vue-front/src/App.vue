842eee942d02d93a7072941433e3882758218f80
Léo Wadin <wadinleo@gmail.com>
2024-04-25 18:12:24 +0200

    feat: navbar

--- vue-front/src/App.vue
+++ vue-front/src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <BNavbar variant="primary" v-b-color-mode="'dark'">
-    <BNavbarBrand tag="h1" class="mb-0">BootstrapVue</BNavbarBrand>
+    <BNavbarBrand tag="h1" class="mb-0">FIS'AIR</BNavbarBrand>
+    <BNavbarNav>
+      <BNavItem href="#">Nos produits</BNavItem>
+      <BNavItem href="#">A propos</BNavItem>
+      <BNavItem href="#">Contact</BNavItem>
+
+    </BNavbarNav>
+
+    <!-- Panier -->
+    <BNavbarNav class="ml-auto">
+      <BNavItem href="#">Panier</BNavItem>
+    </BNavbarNav>
+
   </BNavbar>
 </template>
 
