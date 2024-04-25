7890fe0972b2fb4ae075d18365c12fdefe2fa1a4
Léo Wadin <wadinleo@gmail.com>
2024-04-25 18:02:45 +0200

    dependency: ajout de Bootstrap

--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { createBootstrap } from 'bootstrap-vue-next'
+
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+
+createApp(App).use(router).use(createBootstrap({components: true, directives: true})).mount('#app')
