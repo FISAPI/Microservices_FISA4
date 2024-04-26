4091778d305ee60d2bf4b16ca0614caea26e1e09
Aurélien Houdart <aure.houdart@gmail.com>
2024-04-26 09:36:58 +0200

    page product v2

--- databases/sql/create-db_product.sql
+++ databases/sql/create-db_product.sql
@@ -9,16 +9,16 @@ CREATE TABLE "product" (
 
 -- Peuplement de la table "Product" avec des données d'exemple
 INSERT INTO "product" (title, description, image, prix) VALUES
-                                                            ('Air Auvergne Rhone Alpes', 'Vivez les montagnes et les lacs de la région Auvergne Rhône-Alpes', 'assets/img/Bouteille_ARA.png', 19.99),
-                                                            ('Air Bourgogne Franche Comté', 'Découvrez les vignobles et les forêts de la région Bourgogne Franche-Comté', 'assets/img/Bouteille_BFC.png', 19.99),
-                                                            ('Air Bretagne', 'Respirez l''air marin de la Bretagne et ses côtes sauvages', 'assets/img/Bouteille_Bretagne.png', 19.99),
-                                                            ('Air Centre Val de Loire', 'Plongez au cœur des châteaux de la Loire et de la nature préservée de la région Centre Val de Loire', 'assets/img/Bouteille_CVDL.png', 19.99),
-                                                            ('Air Corse', 'Évadez-vous sur l''île de beauté et ses paysages époustouflants', 'assets/img/Bouteille_Corse.png', 19.99),
-                                                            ('Air Grand Est', 'Partez à la découverte des villages typiques et des montagnes Vosgiennes de la région Grand Est', 'assets/img/Bouteille_GE.png', 19.99),
-                                                            ('Air Hauts de France', 'Découvrez les paysages variés et les villes chargées d''histoire des Hauts-de-France', 'assets/img/Bouteille_HDF.png', 19.99),
-                                                            ('Air Ile de France', 'Dégustez l''air de la capitale et de ses monuments emblématiques', 'assets/img/Bouteille_IDF.png', 19.99),
-                                                            ('Air Normandie', 'Laissez-vous séduire par les falaises et les plages du débarquement de la région Normandie', 'assets/img/Bouteille_Normandie.png', 19.99),
-                                                            ('Air Nouvelle Aquitaine', 'Partez à la rencontre des vagues de l''Atlantique et des vignobles bordelais de la région Nouvelle Aquitaine', 'assets/img/Bouteille_NA.png', 19.99),
-                                                            ('Air Occitanie', 'Découvrez les paysages méditerranéens et les montagnes des Pyrénées de la région Occitanie', 'assets/img/Bouteille_Occitanie.png', 19.99),
-                                                            ('Air Pays de la Loire', 'Respirez l''air iodé des côtes vendéennes et des châteaux de la Loire', 'assets/img/Bouteille_PDL.png', 19.99),
-                                                            ('Air Provence Alpes Côte d''Azur', 'Évadez-vous sur les plages de la Côte d''Azur et les montagnes des Alpes du Sud', 'assets/img/Bouteille_PACA.png', 19.99);
+                                                            ('Air Auvergne Rhone Alpes', 'Vivez les montagnes et les lacs de la région Auvergne Rhône-Alpes', 'img/Bouteille_ARA.png', 19.99),
+                                                            ('Air Bourgogne Franche Comté', 'Découvrez les vignobles et les forêts de la région Bourgogne Franche-Comté', 'img/BouteilleBFC.png', 19.99),
+                                                            ('Air Bretagne', 'Respirez l''air marin de la Bretagne et ses côtes sauvages', 'img/Bouteille_Bretagne.png', 19.99),
+                                                            ('Air Centre Val de Loire', 'Plongez au cœur des châteaux de la Loire et de la nature préservée de la région Centre Val de Loire', 'img/Bouteille_CVL.png', 19.99),
+                                                            ('Air Corse', 'Évadez-vous sur l''île de beauté et ses paysages époustouflants', 'img/Bouteille_Corse.png', 19.99),
+                                                            ('Air Grand Est', 'Partez à la découverte des villages typiques et des montagnes Vosgiennes de la région Grand Est', 'img/Bouteille_Grand_Est.png', 19.99),
+                                                            ('Air Hauts de France', 'Découvrez les paysages variés et les villes chargées d''histoire des Hauts-de-France', 'img/Bouteille_HDF.png', 19.99),
+                                                            ('Air Ile de France', 'Dégustez l''air de la capitale et de ses monuments emblématiques', 'img/Bouteille_IDF.png', 19.99),
+                                                            ('Air Normandie', 'Laissez-vous séduire par les falaises et les plages du débarquement de la région Normandie', 'img/Bouteille_Normandie.png', 19.99),
+                                                            ('Air Nouvelle Aquitaine', 'Partez à la rencontre des vagues de l''Atlantique et des vignobles bordelais de la région Nouvelle Aquitaine', 'img/Bouteille_Nouvelle_Aquitaine.png', 19.99),
+                                                            ('Air Occitanie', 'Découvrez les paysages méditerranéens et les montagnes des Pyrénées de la région Occitanie', 'img/Bouteille_Occitanie.png', 19.99),
+                                                            ('Air Pays de la Loire', 'Respirez l''air iodé des côtes vendéennes et des châteaux de la Loire', 'img/Bouteille_PdlL.png', 19.99),
+                                                            ('Air Provence Alpes Côte d''Azur', 'Évadez-vous sur les plages de la Côte d''Azur et les montagnes des Alpes du Sud', 'img/Bouteille_PACA.png', 19.99);
