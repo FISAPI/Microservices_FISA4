07f5ee783a272ecaf1684bb196f38685e713efd5
Aurélien Houdart <aure.houdart@gmail.com>
2024-04-26 08:35:56 +0200

    ajout product card

---
+++ vue-front/src/components/ProductCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <BCard
+      :title="product.title"
+      :img-src="product.image"
+      :img-alt="product.title"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+  >
+    <BCardText>
+      {{ product.description }}
+      <h5>{{ product.prix }} €</h5>
+    </BCardText>
+
+    <BButton href="#" variant="primary">Voir détails</BButton>
+    <BButton href="#" variant="primary">Ajouter au panier</BButton>
+  </BCard>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      products: [
+        { id: 1, name: "Equality - Jersey 2022", size: "XS", price: 34.90, quantity: 3 }
+        // Ajoute d'autres articles selon ta structure de données
+      ],
+      fields: [
+        { key: "actions", label: "", sortable: false },
+        { key: "thumbnail", label: "Miniature" },
+        { key: "name", label: "Produit" },
+        { key: "price", label: "Prix" },
+        { key: "quantity", label: "Quantité" },
+        { key: "subtotal", label: "Sous-total" }
+      ]
+    };
+}
+
+</script>
---
+++ vue-front/src/views/Products.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">Home page</div>
+  <ProductCard product=""/>
+</template>
+
+<script>
+import ProductCard from "@/components/ProductCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ProductCard,
+  },
+};
+</script>
